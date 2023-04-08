0fb5eb90d5ec18970b9c6f0e07f9c56892da1c58
ttakuma17 <ttanimiya@gmail.com>
2023-04-08 11:35:36 +0900

    notionからデータ取得できることを確認

--- lib/notionAPI.ts
+++ lib/notionAPI.ts
@@ -6,11 +6,33 @@ const notion = new Client({
 
 export const getAllPosts = async () => {
   const posts = await notion.databases.query({
+    // NOTION_DATABASE_ID が string | undefined になってるので、型エラーが出る
     database_id: process.env.NOTION_DATABASE_ID,
     page_size: 100,
   });
 
   const allPosts = posts.results;
 
-  return allPosts;
+  return allPosts.map((post) => {
+    return getPageMetaData(post);
+  });
+};
+
+const getPageMetaData = (post) => {
+  const getTags = (tags) => {
+    const allTags = tags.map((tag) => {
+      return tag.name;
+    });
+
+    return allTags;
+  };
+
+  return {
+    id: post.id,
+    title: post.properties.name.title[0].plain_text,
+    description: post.properties.description.rich_text[0].plain_text,
+    date: post.properties.date.date.start,
+    slug: post.properties.slug.rich_text[0].plain_text,
+    tags: getTags(post.properties.tags.multi_select),
+  };
 };
