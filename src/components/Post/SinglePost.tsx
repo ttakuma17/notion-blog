99f2ff1e68b9cf56b0e357425b89cb79ddb6b496
ttakuma17 <ttanimiya@gmail.com>
2023-04-08 12:16:38 +0900

    ナビゲーションバーを追加

--- src/components/Post/SinglePost.tsx
+++ src/components/Post/SinglePost.tsx
@@ -13,20 +13,20 @@ const SinglePost = (props: Props) => {
   const { title, description, date, tags, slug } = props;
 
   return (
-    <Link href={`/posts/${slug}`}>
-      <section className="lg:w-1/2 bg-sky-600 mb-8 mx-auto ronded-mb p-5 shadow-xl hover:shadow-none hover:translate-y-1 transition-all duration-300">
-        <div className="flex items-center gap-3">
-          <h2 className="text-gray-100 text-2xl font-medium mb-2">{title}</h2>
-          <div className="text-gray-100">{date}</div>
-          {tags.map((tag) => (
-            <span className="text-white bg-gray-400 rounded-xl px-2 pb-1 font-medium">
-              {tag}
-            </span>
-          ))}
-        </div>
-        <p className="text-gray-100">{description}</p>
-      </section>
-    </Link>
+    <section className="lg:w-1/2 bg-sky-600 mb-8 mx-auto ronded-mb p-5 shadow-xl hover:shadow-none hover:translate-y-1 transition-all duration-300">
+      <div className="flex items-center gap-3">
+        <h2 className="text-gray-100 text-2xl font-medium mb-2">
+          <Link href={`/posts/${slug}`}>{title}</Link>
+        </h2>
+        <div className="text-gray-100">{date}</div>
+        {tags.map((tag) => (
+          <span className="text-white bg-gray-400 rounded-xl px-2 pb-1 font-medium">
+            {tag}
+          </span>
+        ))}
+      </div>
+      <p className="text-gray-100">{description}</p>
+    </section>
   );
 };
 
